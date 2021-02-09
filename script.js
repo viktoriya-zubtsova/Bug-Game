81f796b954eeedde38e0f93ccacb723dfa96c26d
Viktoriya Zubtsova <wow55222@yandex.ru>
2021-02-09 15:37:50 +0700

    не могу понять, почему не скрывается loading, в jsfiddle работает как надо, а в браузере выдает ошибку на добавление класса

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let loading = document.getElementById('loading');
+setTimeout(() => loading.classList.add('off'), 5000);
+
 let url = window.location.search;
 function getName(url) {
 	let getUrl = url.split('=');
@@ -7,8 +10,19 @@ function getName(url) {
 	}
 	return name;
 }
-function doFetch() {
-  fetch(`https://api.github.com/users/${getName(url)}`)
+
+let date = new Date();
+let userUrl = `https://api.github.com/users/${getName(url)}`;
+
+let getDate = new Promise((resolve, reject) => {
+	setTimeout(() => date ? resolve(date) : reject('Дата не определена'), 2000)
+});
+let getUrl = new Promise((resolve, reject) => {
+	setTimeout(() => userUrl ? resolve(userUrl) : reject('Ссылка не найдена'), 5000)
+});
+
+Promise.all([getUrl, getDate])
+	.then(([userUrl, date]) => fetch(userUrl))
   .then(res => res.json())
   .then(json => {
     let avatar = document.createElement('img');
@@ -17,22 +31,24 @@ function doFetch() {
     } else {
       document.body.innerHTML= ('Аватар отсутствует<br>');
     }
+    document.body.prepend(avatar);
+
     let link = document.createElement('a');
     link.href = json.html_url;
     if(json.login != null) {
-      link.innerHTML = json.login + '<br>';
+      link.innerHTML = '<br><br>' + json.login + '<br>';
     } else {
       link.innerHTML = 'Нет информации об имени<br>';
     }
-    if(json.bio != null) {
-      document.body.innerHTML= json.bio +'<br>';
-    } else {
-      document.body.innerHTML= ('Нет информации о биографии<br>');
-    }
     document.body.append(link);
-    document.body.append(avatar);
+
+		let bio = document.createElement('p');
+    if(json.bio != null) {
+      bio.innerHTML= json.bio +'<br>';
+    } else {
+      bio.innerHTML= ('Нет информации о биографии<br>');
+    }
+		document.body.append(bio);
+    document.body.append(date);
     })
   .catch(err => document.body.innerHTML = ('Информация о пользователе недоступна'));
-}
-doFetch(name);
-console.log(window.location.search);
